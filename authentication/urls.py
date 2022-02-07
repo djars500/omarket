fb2c5e12fb29d947f188b9b8f8ad5428bd6af2d5
root <root@797145-cm08224.tmweb.ru>
2022-02-07 13:06:22 +0300

    2

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import login
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', login)
+    path('', login),
+    path('logout/', LogoutView.as_view(), name = 'logout')
 ]
